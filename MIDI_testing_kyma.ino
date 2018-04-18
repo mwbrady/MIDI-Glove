d6880cc25b4f415e5af81aacdf18cc41e3d0e8ff
mwbrady <mwbradyhome@comcast.net>
2018-04-18 16:11:08 -0500

    Add files via upload

---
+++ MIDI_testing_kyma.ino
@@ -0,0 +1,27 @@
+#include <MIDI.h>
+
+MIDI_CREATE_DEFAULT_INSTANCE();
+
+void setup() 
+{
+    MIDI.begin(MIDI_CHANNEL_OMNI);
+    Serial.begin(115200);
+}
+
+void loop() 
+{
+  for (int i = 10; i < 80; i++) 
+  {
+    MIDI.sendControlChange(31, i, 1); //Brightness
+    delay(100);
+    MIDI.sendControlChange(32, i, 1); //Decay
+    delay(100);
+    MIDI.sendControlChange(33, i, 1); //Delay
+    delay(100);
+    MIDI.sendControlChange(34, i, 1); //Feedback
+    delay(100);
+    MIDI.sendControlChange(35, i, 1); //Level
+    delay(100);
+  }
+
+}
